02218f70697a4a9e202f7ac08c8df9e05d9ed6ba
Tirth Patel <tirthpatel.1842000@gmail.com>
2022-01-08 16:28:46 +0530

    login,register page added with style

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 // login page
-router.get('/login', (req,res) => res.send('Login'));
+router.get('/login', (req,res) => res.render('login'));
 
 // Register page
-router.get('/Register', (req,res) => res.send('Register'));
+router.get('/Register', (req,res) => res.render('register'));
 
 module.exports = router;
