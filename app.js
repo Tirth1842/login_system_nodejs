cb7a34a114391c0503a30b97e2257fa9a8d35b44
Tirth Patel <tirthpatel.1842000@gmail.com>
2022-01-08 18:09:45 +0530

    validaton, hashing, schema of DB

--- app.js
+++ app.js
@@ -3,11 +3,42 @@ const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const index_routes = require('./routes/index');
 const users_routes = require('./routes/users');
+const flash = require('connect-flash');
+const session = require('express-session');
 
+const mongoose = require('mongoose');
+
+// DB config
+const db = require('./config/keys').MongoURI;
+
+// connect to Mongo
+mongoose.connect(db, {useNewUrlParser: true })
+    .then(() => console.log('mongo db connected..'))
+    .catch(err => console.log(err));
 // ejs
 app.use(expressLayouts);
 app.set('view engine','ejs');
 
+// Bodyparser
+app.use(express.urlencoded({ extended: false}));
+
+// Express Session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+  }))
+
+//connect flash
+app.use(flash());
+
+
+// Global Variable
+app.use((req,res,next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 // Routes
 app.use('/', index_routes);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    MongoURI : 'mongodb+srv://tirth:test1234@cluster0.0hmuc.mongodb.net/passportapp?retryWrites=true&w=majority'
+}
