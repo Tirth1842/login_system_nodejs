2a08d8d65208227cf0021bbcdf241c956b1216da
Tirth Patel <tirthpatel.1842000@gmail.com>
2022-01-09 12:22:21 +0530

    authentication, logout, passport

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
-
+const passport = require('passport');
 // login page
 router.get('/login', (req,res) => res.render('login'));
 
@@ -79,5 +79,21 @@ router.post('/register',(req,res) => {
          })
 
     }
+});
+
+// login Handle
+router.post('/login', (req,res, next) =>{
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true // automatically generates the error.
+    })(req,res,next);
+});
+
+// Logout Handle
+router.get('/logout', (req,res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
 });
 module.exports = router;
